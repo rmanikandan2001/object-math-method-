2958aa24c3afa4f526d0d1c0c1071d06462b0fe1
rmanikandan2001 <rmanikandan6867@gmail.com>
2023-08-25 04:00:26 -0700

    add

---
+++ main.js
@@ -0,0 +1,56 @@
+// math method 
+
+ 
+	let a=15.4
+	
+	let b=15.6
+	
+	let c=15.1
+	
+	 console.log(Math.round(a));
+	 
+	 console.log(Math.floor(b));
+	 
+	 console.log(Math.ceil(c));
+	 
+	 
+	 
+//sign
+
+		let d=15
+	
+		let e=-15 
+		
+		let f=0
+		
+		console.log(Math.sign(d));
+		
+		console.log(Math.sign(e));
+		
+		console.log(Math.sign(f));
+		
+		
+// abs method	 
+
+
+			let g=-43
+			
+			
+			console.log(Math.abs(g));
+			
+			
+// max method
+
+    console.log(Math.max(1,2,3,8,7))	
+
+
+// max method
+
+    console.log(Math.min(1,2,3,-1,5))	
+
+//random method
+ 
+	console.log(Math.random());	
+	
+	console.log(Math.random());	
+		
